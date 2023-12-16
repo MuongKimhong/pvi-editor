94234943ad9e621ac7e96c1a185bc35e71dbd1b4
MuongKimhong <muongkimhong086@gmail.com>
2023-12-16 22:46:10 +1100

    create utils, editor screen

---
+++ pvi/editor.py
@@ -0,0 +1,34 @@
+from textual.widgets import Static, ListView, ListItem
+from textual.containers import Container
+from textual.app import ComposeResult
+from textual.screen import Screen
+from textual import events, log
+
+from components.sidebar import Sidebar
+
+import os
+
+
+class Editor(Screen):
+    CSS_PATH = "style.tcss"
+    dir_tree_listview = ListView(*[], id="dir_tree_listview")
+
+    def compose(self) -> ComposeResult:
+        yield Sidebar()
+        yield Static("Two", id="editor")
+
+    def toggle_sidebar(self) -> None:
+        width = self.query_one(Sidebar).styles.width.value
+
+        self.query_one(Sidebar).styles.width = 0 if width == 30 else 30
+        self.query_one(Sidebar).styles.border = ("hidden", "grey") if width == 30 else ("round", "grey")
+
+    def on_key(self, event: events.Key) -> None:
+        if event.key == "ctrl+b":
+            self.toggle_sidebar()    
+
+    def on_screen_resume(self, event: events.ScreenResume) -> None:
+        pass
+
+    def on_screen_suspend(self, event: events.ScreenSuspend) -> None:
+        pass
--- pvi/main.py
+++ pvi/main.py
@@ -2,26 +2,13 @@ from textual.widgets import TextArea, Static
 from textual.app import App, ComposeResult
 from textual import events, log
 
-
-from components.sidebar import Sidebar
+from editor import Editor
 
 
 class Main(App):
-    CSS_PATH = "style.tcss"
-
-    def compose(self) -> ComposeResult:
-        yield Sidebar()
-        yield Static("Two", id="editor")
-
-    def toggle_sidebar(self) -> None:
-        width = self.query_one(Sidebar).styles.width.value
-
-        self.query_one(Sidebar).styles.width = 0 if width == 30 else 30
-        self.query_one(Sidebar).styles.border = ("hidden", "grey") if width == 30 else ("round", "grey")
-
-    def on_key(self, event: events.Key) -> None:
-        if event.key == "ctrl+b":
-            self.toggle_sidebar()        
+    def on_mount(self):
+        self.install_screen(Editor, "editor")
+        self.push_screen("editor")
 
 
 if __name__ == "__main__":
