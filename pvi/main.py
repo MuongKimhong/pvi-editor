221689a323b256143ea2209ef8f5d2cd6c71a203
MuongKimhong <muongkimhong086@gmail.com>
2023-12-17 11:18:03 +1100

    cli arguments

--- pvi/main.py
+++ pvi/main.py
@@ -4,13 +4,40 @@ from textual import events, log
 
 from editor import Editor
 
+import argparse
+import os
+
 
 class Main(App):
+    def __init__(self, cli_argument):
+        self.cli_argument = cli_argument
+        super().__init__()
+
     def on_mount(self):
+        log("Hello world")
+        if self.cli_argument["path"] == ".":
+            working_directory = os.getcwd()
+            log(working_directory)
+        elif os.path.exists(self.cli_argument["path"]) is False:
+            raise Exception( f'''
+            Provided path ({self.cli_argument['path']}) is not exists!
+            Please provide a valid path
+            ''')
+        elif os.path.isfile(self.cli_argument["path"]):
+            editing_type = "file"
+        elif os.path.isdir(self.cli_argument["path"]):
+            editing_type = "dir"
+
+        working_directory = self.cli_argument["path"]
+
         self.install_screen(Editor, "editor")
         self.push_screen("editor")
 
 
 if __name__ == "__main__":
-    app = Main()
+    arg = argparse.ArgumentParser()
+    arg.add_argument("-p", "--path", help="Path to working directory", required=True)
+    arguments = vars(arg.parse_args())
+
+    app = Main(cli_argument=arguments)
     app.run()
