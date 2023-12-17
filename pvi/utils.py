a09df736760e0bdd2e07307857f252c8af067d2a
MuongKimhong <muongkimhong086@gmail.com>
2023-12-17 11:18:26 +1100

    create ini file to store and access global variables

--- pvi/utils.py
+++ pvi/utils.py
@@ -1,5 +1,20 @@
 from pathlib import Path
+import configparser
 
 
-def get_project_root() -> Path:
+def get_pvi_root() -> Path:
     return Path(__file__).parent.parent
+
+
+def read_ini_file(section_name: str) -> dict:
+    config = configparser.ConfigParser()
+    config.read("store/stores.ini")
+    return config[section_name] 
+
+
+def update_ini_file(section_name: str, section_data: dict) -> None:
+    config = configparser.ConfigParser()
+    config[section_name].update(section_data)
+
+    with open("store/stores.ini", "w") as configfile:
+        config.write(configfile)
