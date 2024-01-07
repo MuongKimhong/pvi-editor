11490a6ec026aa92020c6d2cf28dcdf6f2c02b97
MuongKimhong <muongkimhong086@gmail.com>
2024-01-07 20:08:42 +1100

    experiment autocomplete

--- pvi/components/text_area.py
+++ pvi/components/text_area.py
@@ -1,9 +1,15 @@
-from textual.widgets import TextArea
+from textual.widgets import TextArea, Static
 from textual import events, log
 
 
 class PviTextArea(TextArea):
     autocomplete_symbol = ['"', '{', '[', '(', '`', "'"]
+
+    DEFAULT_CSS = """
+    PviTextArea {
+        layers: above;
+    } 
+    """
 
     def handle_autocomplete_symbol(self, character) -> None:
         if character == "{":
@@ -28,3 +34,15 @@ class PviTextArea(TextArea):
 
         elif event.character in self.autocomplete_symbol:
             self.handle_autocomplete_symbol(character=event.character)
+
+        elif event.key == "s":
+            # static = Static("Good evening", id="text")
+            # # static.styles.layer = "above"
+            # # static.styles.width = 50
+            # # static.styles.height = 10
+            # # static.styles.content_align = ("center", "middle")
+            # # static.styles.offset = (13, 6)
+            # # static.styles.background = "yellow"
+            # # static.styles.color = "white"
+            # self.mount(static)
+            # static.scroll_visible()
