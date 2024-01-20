c99f995d6b833a5a583da17df1829195a54f16ed
MuongKimhong <muongkimhong086@gmail.com>
2024-01-20 15:48:20 +1100

    update main.py and pvi.sh

--- pvi.sh
+++ pvi.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+
+if [ "$#" -eq 0 ]; then
+    echo -e "${RED}\n[PVI ERROR] No file or directory provided."
+    echo -e "${CYAN}- <pvi file.txt> open file.txt and edit"
+    echo -e "${CYAN}- <pvi directory> open directory"
+    echo -e "${CYAN}- <pvi .> open current directory"   
+    exit 0
+fi
+
 pvi_directory="$HOME/pvi-editor"
 
-cd "$pvi_directory" || exit 1
+# pvi-editor exists
+if [ -e "$pvi_directory" ]; then
+    before_change=$(pwd)
+    arguments=("$before_change"/"$@")
 
-entry_point_script="$pvi_directory/pvi/main.py"
-
-# Use "$@" to pass all the arguments provided to the script
-poetry run python "$entry_point_script" "$@"
+    # check if provided file or directory path exists
+    if [ -e "${arguments[@]}" ]; then
+        cd "$pvi_directory"
+        entry_point="$pvi_directory/pvi/main.py"
+        poetry run python "$entry_point" "${arguments[@]}"
+    fi
+else
+    echo -e "${RED}\n[PVI ERROR] pvi-editor not found. Please install..${NC}"
+    exit 0
+fi
--- pvi/main.py
+++ pvi/main.py
@@ -22,43 +22,67 @@ class Main(App):
             "project_root": None, 
             "argument_parser_type": None
         }
-        # pvi -d some_directory
-        if self.cli_argument["directory"]:
-            if os.path.exists(self.cli_argument["directory"]) and os.path.isdir(self.cli_argument["directory"]):
-                section_data["editing_path"] = f"{os.getcwd()}/{self.cli_argument['directory']}"
-                section_data["project_root"] = f"{os.getcwd()}/{self.cli_argument['directory']}"
-                section_data["editing_type"] = "dir"
-                section_data["argument_parser_type"] = "dir"
-            else:
-                raise Exception(f'''
-                \n[Error] No directory found. "{self.cli_argument['file_or_current_directory']}".\n 
-                ''')
+        # # pvi -d some_directory
+        # if self.cli_argument["directory"]:
+        #     if os.path.exists(self.cli_argument["directory"]) and os.path.isdir(self.cli_argument["directory"]):
+        #         section_data["editing_path"] = f"{os.getcwd()}/{self.cli_argument['directory']}"
+        #         section_data["project_root"] = f"{os.getcwd()}/{self.cli_argument['directory']}"
+        #         section_data["editing_type"] = "dir"
+        #         section_data["argument_parser_type"] = "dir"
+        #     else:
+        #         raise Exception(f'''
+        #         \n[Error] No directory found. "{self.cli_argument['file_or_current_directory']}".\n 
+        #         ''')
         
-        elif self.cli_argument["file_or_current_directory"]:
-            # pvi .  : open current directory
-            if self.cli_argument["file_or_current_directory"] == ".":
-                section_data["editing_path"] = os.getcwd()
-                section_data["project_root"] = os.getcwd()
-                section_data["editing_type"] = "dir"
-                section_data["argument_parser_type"] = "dir"
+        # elif self.cli_argument["file_or_current_directory"]:
+        #     # pvi .  : open current directory
+        #     if self.cli_argument["file_or_current_directory"] == ".":
+        #         section_data["editing_path"] = os.getcwd()
+        #         section_data["project_root"] = os.getcwd()
+        #         section_data["editing_type"] = "dir"
+        #         section_data["argument_parser_type"] = "dir"
 
-            else: 
-                # pvi somefile.py : open somefile.py
-                if os.path.exists(self.cli_argument["file_or_current_directory"]):
-                    if os.path.isfile(self.cli_argument["file_or_current_directory"]):
-                        section_data["editing_path"] = f"{os.getcwd()}/{self.cli_argument['file_or_current_directory']}"
-                        section_data["project_root"] = os.getcwd()
-                        section_data["editing_type"] = "file"
-                        section_data["argument_parser_type"] = "file"
-                else:
-                    raise Exception(f'''
-                    \n[Error] No such file "{self.cli_argument['file_or_current_directory']}".\n 
-                    ''')
+        #     else: 
+        #         # pvi somefile.py : open somefile.py
+        #         if os.path.exists(self.cli_argument["file_or_current_directory"]):
+        #             if os.path.isfile(self.cli_argument["file_or_current_directory"]):
+        #                 section_data["editing_path"] = f"{os.getcwd()}/{self.cli_argument['file_or_current_directory']}"
+        #                 section_data["project_root"] = os.getcwd()
+        #                 section_data["editing_type"] = "file"
+        #                 section_data["argument_parser_type"] = "file"
+        #         else:
+        #             raise Exception(f'''
+        #             \n[Error] No such file "{self.cli_argument['file_or_current_directory']}".\n 
+        #             ''')
         
-        else:
-            raise Exception('''
-            \n[Error] The provided argument is not supported! Please check the documentation for more detail.\n
-            ''')
+        # else:
+        #     raise Exception('''
+        #     \n[Error] The provided argument is not supported! Please check the documentation for more detail.\n
+        #     ''')
+        # update_ini_file(file_name="stores.ini", section_name="WorkingDirectory", section_data=section_data)
+
+        # self.install_screen(Editor, "editor")
+        # self.push_screen("editor")
+        argument = self.cli_argument["argument"]
+        argument_split = self.cli_argument["argument"].split("/")[1:]
+
+        if argument_split[-1] == ".":
+            section_data["editing_path"] = argument[:-2] # remove /. from arguemnt
+            section_data["project_root"] = argument[:-2]
+            section_data["editing_type"] = "dir"
+            section_data["argument_parser_type"] = "dir"
+        elif os.path.isdir(argument):
+            # remove / from the end of argument if any
+            section_data["editing_path"] = argument[:-1] if argument[-1] == "/" else argument
+            section_data["project_root"] = argument[:-1] if argument[-1] == "/" else argument
+            section_data["editing_type"] = "dir"
+            section_data["argument_parser_type"] = "dir"
+        elif os.path.isfile(argument):
+            section_data["editing_path"] = argument
+            section_data["project_root"] = "/" + "/".join(argument_split[:-1])
+            section_data["editing_type"] = "file"
+            section_data["argument_parser_type"] = "file"
+
         update_ini_file(file_name="stores.ini", section_name="WorkingDirectory", section_data=section_data)
 
         self.install_screen(Editor, "editor")
@@ -67,8 +91,10 @@ class Main(App):
 
 if __name__ == "__main__":
     arg = argparse.ArgumentParser()
-    arg.add_argument("-d", "--directory", help="Open a directory", required=False)
-    arg.add_argument("file_or_current_directory", nargs="?", help="a file to edit or a current directory to open")
+    # arg.add_argument("-d", "--directory", help="Open a directory", required=False)
+    # arg.add_argument("file_or_current_directory", nargs="?", help="a file to edit or a current directory to open")
+
+    arg.add_argument("argument", nargs="?", help="a file or directory or current directory")
     arguments = vars(arg.parse_args())
 
     app = Main(cli_argument=arguments)
