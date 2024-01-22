528c2c470ba94631f26ccd13f0a72a4307e1a531
MuongKimhong <muongkimhong086@gmail.com>
2024-01-22 18:34:03 +1100

    move WelcomeText style to seperate file

--- pvi/components/welcome_text.py
+++ pvi/components/welcome_text.py
@@ -2,9 +2,12 @@ from textual.containers import Container
 from textual.app import ComposeResult
 from textual.widgets import Static
 
+from utils import get_pvi_root
+
 
 class WelcomeText(Container):
-    CSS_PATH = "../styles/style.tcss"
+    with open(f"{get_pvi_root()}/pvi/styles/welcome_text_style.tcss", "r") as file:
+        DEFAULT_CSS = file.read()
 
     def compose(self) -> ComposeResult:
         yield Static("Welcome to PVI - Python Vim Editor\n", id="welcome")
